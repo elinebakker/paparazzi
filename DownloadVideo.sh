e77517e905eb32af7362e3c79d2b8ae4ed4070f1
Geart van Dam <geartvd@gmail.com>
2017-03-11 13:38:18 +0100

    Fixed the name of the downloaded file to include current date and time

--- DownloadVideo.sh
+++ DownloadVideo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-_now=$(date + "%m_%d_%Y")
+now=`date '+%Y_%m_%d__%H_%M_%S'`
 ffmpeg -analyzeduration 100M -probesize 10M -c h264 -an -sn -i ftp://192.168.42.1/internal_000/recordedVideo.h264 -vcodec libx264 -ss 1 -map_chapters -1 -map_metadata -1 -an -sn ~/Videos/bebop_$now.mp4
 
