a3bb6d1096ff84434b249ad55646c58985721a08
Geart van Dam <geartvd@gmail.com>
2017-03-15 18:02:41 +0100

    Created a simple ground following vision module. Note that some of the functions still need to be written.

---
+++ sw/airborne/modules/ground_follower/ground_follower.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) Geart van Dam
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/ground_follower/ground_follower.h"
+ * @author Geart van Dam
+ * Identify the ground and follow it
+ */
+
+#ifndef GROUND_FOLLOWER_H
+#define GROUND_FOLLOWER_H
+#include <inttypes.h>
+#include "state.h"
+
+extern uint8_t orange_avoider_safeToGoForwards;
+
+extern void ground_follower_init(void);
+
+extern bool checkIfSafeToGoForwards(void);
+
+extern struct image_t *calculateOptionMatrix(struct image_t *input_img);
+
+float findPercentageGround(int x_min, int x_max, int y_min, int y_max);
+
+float findBestDirection(void);
+
+void updateGroundFilterSettings(void);
+
+#endif
+
